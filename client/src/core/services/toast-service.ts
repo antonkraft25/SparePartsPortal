37fed44f6056215864ee9b3fbfb9f6895c73eec5
antonkraft25 <antonkraft25@gmail.com>
2026-05-16 11:42:47 +0200

    Fixes so you could only send up one toast message at a time, so the site doesnt break. #30

--- client/src/core/services/toast-service.ts
+++ client/src/core/services/toast-service.ts
@@ -4,59 +4,63 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ToastService {
-
   constructor() {
     this.createToastContainer();
   }
 
   private createToastContainer() {
-    if(!document.getElementById('toast-container')) {
+    if (!document.getElementById('toast-container')) {
       const container = document.createElement('div');
       container.id = 'toast-container';
-      container.className = 'toast toast-bottom toast-end'
+      container.className = 'toast toast-bottom toast-end';
       document.body.appendChild(container);
     }
   }
 
+  private clearToasts() {
+    const toastContainer = document.getElementById('toast-container');
+    if (toastContainer) toastContainer.innerHTML = '';
+  }
+
   private createToastElement(message: string, alertClass: string, duration = 5000) {
     const toastContainer = document.getElementById('toast-container');
-    if(!toastContainer) return;
+    if (!toastContainer) return;
+
+    this.clearToasts();
 
     const toast = document.createElement('div');
     toast.classList.add('alert', alertClass, 'shadow-lg');
     toast.innerHTML = `
       <span>${message}</span>
       <button class="ml-4 btn btn-sm btn-ghost">x</button>
-    `
+    `;
 
-    toast.querySelector('button')?.addEventListener('click', () => {      
+    toast.querySelector('button')?.addEventListener('click', () => {
       toastContainer.removeChild(toast);
-    })
+    });
 
     toastContainer.appendChild(toast);
 
     setTimeout(() => {
-      if(toastContainer.contains(toast)) {
+      if (toastContainer.contains(toast)) {
         toastContainer.removeChild(toast);
       }
     }, duration);
-        
   }
 
   success(message: string, duration?: number) {
-    this.createToastElement(message, 'alert-success', duration); 
+    this.createToastElement(message, 'alert-success', duration);
   }
 
   error(message: string, duration?: number) {
-    this.createToastElement(message, 'alert-error', duration); 
+    this.createToastElement(message, 'alert-error', duration);
   }
 
   warning(message: string, duration?: number) {
-    this.createToastElement(message, 'alert-waring', duration); 
+    this.createToastElement(message, 'alert-waring', duration);
   }
 
   info(message: string, duration?: number) {
-    this.createToastElement(message, 'alert-info', duration); 
+    this.createToastElement(message, 'alert-info', duration);
   }
-  
 }
